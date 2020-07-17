dd5ea18d9605b4ec0dd1aefbe61f813d75af0d88
Andrew Ralston <andrewralston35@gmail.com>
2020-07-17 19:26:17 +0000

    add record now functional

--- app.py
+++ app.py
@@ -24,6 +24,37 @@ def add_record():
     return render_template("addrecord.html", genres=mongo.db.genres.find())
 
 
+@app.route('/insert_record', methods=["POST"])
+def insert_record():
+    records = mongo.db.records
+    records.insert_one(request.form.to_dict())
+    return redirect(url_for('get_records'))
+
+
+@app.route('/edit_record/<record_id>')
+def edit_record(record_id):
+    the_record = mongo.db.records.find_one({'_id': ObjectId(record_id)})
+    all_genres = mongo.db.genres.find()
+    return render_template('editrecord.html',
+                           record=the_record, genre=all_genres)
+
+
+@app.route('/update_record/<record_id>', methods=["POST"])
+def update_record(record_id):
+    records = mongo.db.records
+    records.update({'_id': ObjectId(record_id)},
+                   {
+                        'record_title': request.form.get('record_title'),
+                        'artist_name': request.form.get('artist_name'),
+                        'label': request.form.get('label'),
+                        'year': request.form.get('year'),
+                        'genre': request.form.get('genre'),
+                        'get_it_here': request.form.get('get_it_here'),
+                        'star_rating': request.form.get('star_rating')
+                   })
+    return redirect(url_for('get_records'))
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
