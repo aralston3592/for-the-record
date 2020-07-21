06c60741bca96d47dbde2adebe6faa042877c7c8
Andrew Ralston <andrewralston35@gmail.com>
2020-07-21 20:14:43 +0000

    register/login user functionality added

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+import bcrypt
 if os.path.exists("env.py"):
     import env
 
@@ -14,6 +15,46 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
+def index():
+    if 'username' in session:
+        return render_template('records.html')
+    
+    return render_template('register.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    users = mongo.db.users
+    login_user = users.find_one({'name': request.form['username']})
+
+    if login_user:
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'),
+                         login_user['password']) == login_user['password']:
+            session['username'] = request.form['username']
+            return redirect(url_for('get_records'))
+
+    return 'Invalid username or password'
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'username': request.form['username']})
+
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'),
+                                     bcrypt.gensalt())
+            users.insert({'username': request.form['username'],
+                          'password': hashpass})
+            session['username'] = request.form['username']
+            return redirect(url_for('get_records'))
+
+        return 'That username already exists!'
+
+    return render_template('register.html')
+
+
 @app.route('/get_records')
 def get_records():
     return render_template("records.html", records=mongo.db.records.find())
@@ -89,8 +130,8 @@ def get_records_in_genre(genre):
                            records=mongo.db.records.find())
 
 
-
 if __name__ == "__main__":
+    app.secret_key = 'ralston35'
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
